e1277eb35cb7453ce1cbf16b3292b71ac0f7fecd
Beomjun Kim <bjkim@yc.ac.kr>
2025-12-05 07:04:17 +0900

    docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/emotiondiary/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [
     // react({
     //   babel: {
