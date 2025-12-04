de0735154d7c2b32fb24f36da41d998933bba04e
Beomjun Kim <bjkim@yc.ac.kr>
2025-12-05 07:18:28 +0900

    Edit for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/emotiondiary/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/emotiondiary/" : "/",
   build: {
     outDir: "docs",
   },
@@ -15,4 +15,4 @@ export default defineConfig({
     // }),
     react(),
   ],
-});
+}));
