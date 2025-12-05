6da40e87e37c3959effeb93079a59fdf4e6f592b
I-203 <smartsw@yc.ac.kr>
2025-12-05 09:48:06 +0900

    test4

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/emotiondiary/" : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/emotiondiary/" : "/",
   build: {
     outDir: "docs",
   },
